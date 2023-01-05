13065228a74d5e7298dbcf55332748a7addec84c
Rodrigo Araujo <rod.dearaujo@gmail.com>
2023-01-05 15:18:57 -0800

    More gh integration

--- init.lua
+++ init.lua
@@ -261,7 +261,7 @@ return require('packer').startup(function(use)
             submit_comment = "<C-s>",
             -- inside a convo buffer, when your cursor is ontop of a comment, open
             -- up a set of actions that can be performed.
-            actions = "<C-a>",
+            actions = "<C-b>",
             -- inside a thread convo buffer, resolve the thread.
             resolve_thread = "<C-r>",
             -- inside a gh.nvim panel, if possible, open the node's web URL in your
@@ -271,10 +271,74 @@ return require('packer').startup(function(use)
         }
     })
 
+    -- Github integration
     use {
         'ldelossa/gh.nvim',
         requires = { { 'ldelossa/litee.nvim' } }
     }
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    local wk = require("which-key")
+    wk.register({
+        g = {
+            name = "+Git",
+            h = {
+                name = "+Github",
+                c = {
+                    name = "+Commits",
+                    c = { "<cmd>GHCloseCommit<cr>", "Close" },
+                    e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+                    o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+                    p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+                    z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+                },
+                i = {
+                    name = "+Issues",
+                    p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+                },
+                l = {
+                    name = "+Litee",
+                    t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+                },
+                r = {
+                    name = "+Review",
+                    b = { "<cmd>GHStartReview<cr>", "Begin" },
+                    c = { "<cmd>GHCloseReview<cr>", "Close" },
+                    d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+                    e = { "<cmd>GHExpandReview<cr>", "Expand" },
+                    s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+                    z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+                },
+                p = {
+                    name = "+Pull Request",
+                    c = { "<cmd>GHClosePR<cr>", "Close" },
+                    d = { "<cmd>GHPRDetails<cr>", "Details" },
+                    e = { "<cmd>GHExpandPR<cr>", "Expand" },
+                    o = { "<cmd>GHOpenPR<cr>", "Open" },
+                    p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+                    r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+                    t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+                    z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+                },
+                t = {
+                    name = "+Threads",
+                    c = { "<cmd>GHCreateThread<cr>", "Create" },
+                    n = { "<cmd>GHNextThread<cr>", "Next" },
+                    t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+                },
+            },
+        },
+    }, { prefix = "<leader>" })
 
     vim.cmd [[let g:gruvbox_material_background = 'soft']]
     vim.cmd([[ colorscheme gruvbox-material]])
